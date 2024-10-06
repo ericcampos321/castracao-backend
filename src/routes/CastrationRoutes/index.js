0314359f7e2a674d04e1b0e229a58153965f03af
ericcampos321 <ericaguiar_@hotmail.com>
2024-10-06 02:29:21 -0300

    fix/AdjustmentsCityController

--- src/routes/CastrationRoutes/index.js
+++ src/routes/CastrationRoutes/index.js
@@ -3,5 +3,8 @@ const router = require('express').Router();
 const CastrationController = require('../../controllers/CastrationController');
 
 router.post('/insertCads', CastrationController.insertCads);
+router.get('/getCadsList/:id', CastrationController.getCads);
+router.patch('/updateCads/:id', CastrationController.updateCads);
+router.delete('/deleteCads/:id', CastrationController.deleteCads);
 
 module.exports = router;
--- src/routes/CityRoutes/index.js
+++ src/routes/CityRoutes/index.js
@@ -3,5 +3,8 @@ const router = require('express').Router();
 const CityController = require('../../controllers/CityController');
 
 router.post('/insertCity', CityController.insertCity);
+router.get('/getCity/:id', CityController.getCity);
+router.delete('/deleteCity/:id', CityController.deleteCity);
+router.patch('/updateCity/:id', CityController.updateCity);
 
 module.exports = router;
